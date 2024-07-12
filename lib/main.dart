e8a4f45f99c903b748fde2d31bf2a854c201bb38
Otana <emmaodongo10@gmail.com>
2024-07-12 17:20:48 +0300

    working on backend

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,17 @@
+import 'package:cjb/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:cjb/pages/splash/splash_page.dart';
-
 import 'pages/onboarding/on_boarding_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
