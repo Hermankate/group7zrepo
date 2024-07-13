59b854e47f83539af4da665aa56ffa205061f490
herman katende <hermankats16@gmail.com>
2024-07-13 17:59:51 +0300

    working on backend

--- lib/pages/auth/auth_service.dart
+++ lib/pages/auth/auth_service.dart
@@ -1,39 +1,88 @@
-import 'dart:developer';
+// import 'dart:developer';
 
+// import 'package:firebase_auth/firebase_auth.dart';
+
+// class AuthService {
+//   final _auth = FirebaseAuth.instance;
+
+//   Future<User?> createUserWithEmailAndPassword(
+//       String email, String password) async {
+//     try {
+//       final cred = await _auth.createUserWithEmailAndPassword(
+//           email: email, password: password);
+//       return cred.user;
+//     } catch (e) {
+//       log("Something went wrong");
+//     }
+//     return null;
+//   }
+
+//   Future<User?> loginUserWithEmailAndPassword(
+//       String email, String password) async {
+//     try {
+//       final cred = await _auth.signInWithEmailAndPassword(
+//           email: email, password: password);
+//       return cred.user;
+//     } catch (e) {
+//       log("Something went wrong");
+//     }
+//     return null;
+//   }
+
+//   Future<void> signout() async {
+//     try {
+//       await _auth.signOut();
+//     } catch (e) {
+//       log("Something went wrong");
+//     }
+//   }
+// }
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:cjb/pages/auth/auth_funcs.dart';
 
-class AuthService {
-  final _auth = FirebaseAuth.instance;
-
-  Future<User?> createUserWithEmailAndPassword(
-      String email, String password) async {
+class AuthServices {
+  static Future<void> signupUser(
+      String email, String password, String name, BuildContext context) async {
     try {
-      final cred = await _auth.createUserWithEmailAndPassword(
-          email: email, password: password);
-      return cred.user;
+      UserCredential userCredential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password);
+
+      await FirebaseAuth.instance.currentUser!.updateDisplayName(name);
+      await FirestoreServices.saveUser(name, email, userCredential.user!.uid);
+
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('Registration Successful')));
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Password Provided is too weak')));
+      } else if (e.code == 'email-already-in-use') {
+        ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Email Provided already Exists')));
+      }
     } catch (e) {
-      log("Something went wrong");
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text(e.toString())));
     }
-    return null;
   }
 
-  Future<User?> loginUserWithEmailAndPassword(
-      String email, String password) async {
+  static Future<void> signinUser(
+      String email, String password, BuildContext context) async {
     try {
-      final cred = await _auth.signInWithEmailAndPassword(
-          email: email, password: password);
-      return cred.user;
-    } catch (e) {
-      log("Something went wrong");
-    }
-    return null;
-  }
+      await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
 
-  Future<void> signout() async {
-    try {
-      await _auth.signOut();
-    } catch (e) {
-      log("Something went wrong");
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('You are Logged in')));
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('No user Found with this Email')));
+      } else if (e.code == 'wrong-password') {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text('Password did not match')));
+      }
     }
   }
 }
