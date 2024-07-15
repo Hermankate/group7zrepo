3cb703853136b25e62c849aef9ae84ccda78e3f9
Jonathan <jonathankiriibwa885@gmail.com>
2024-07-15 09:05:21 +0300

    working on notifications

--- lib/pages/main/notifications/notification.dart
+++ lib/pages/main/notifications/notification.dart
@@ -1,13 +1,19 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
+import 'package:cjb/pages/main/notifications/push_services.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'push_notification_service.dart';
 
-class Notification_Page extends StatelessWidget {
+class Notification_Page extends StatefulWidget {
   const Notification_Page({super.key});
+
+  @override
+  State<Notification_Page> createState() => _Notification_PageState();
+}
+
+class _Notification_PageState extends State<Notification_Page> {
   void initState() {
     super.initState();
     PushNotificationService().initialize();
