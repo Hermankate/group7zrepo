8ea30fd31188f4b57e7a3ef5b0007437e9ee138e
mcxeirols@gmail.com <hermankats16@gmail.com>
2024-07-15 09:02:37 +0300

    working on notifications

--- lib/pages/main/notifications/notification.dart
+++ lib/pages/main/notifications/notification.dart
@@ -2,9 +2,16 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'push_notification_service.dart';
 
 class Notification_Page extends StatelessWidget {
   const Notification_Page({super.key});
+  void initState() {
+    super.initState();
+    PushNotificationService().initialize();
+  }
 
   @override
   Widget build(BuildContext context) {
