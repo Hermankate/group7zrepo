2d29665694c2eeac9761b6c64020815633648586
Jonathan <ranianabatanzi004@gmail.com>
2024-08-09 10:56:14 +0300

    working on ui

--- lib/pages/main/main_page/jobs.dart
+++ lib/pages/main/main_page/jobs.dart
@@ -2,7 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:flutter/material.dart';
+//import 'package:flutter/material.dart';
 
 class Jobs extends StatelessWidget {
   const Jobs({super.key});
